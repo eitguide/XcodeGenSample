9fc53e45786c7340fed822dd991270b714d94aeb
Nguyen Nghia <nghiaklpro195@gmail.com>
2020-04-23 16:34:10 +0700

    first commit

---
+++ Bridge/Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Bridging-Header.h
+//  XcodeGenSample
+//
+//  Created by Nguyen Van Nghia on 4/23/20.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
---
+++ XcodeGenSample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  XcodeGenSample
+//
+//  Created by Nguyen Van Nghia on 4/23/20.
+//  Copyright © 2020 Nguyen Van Nghia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
